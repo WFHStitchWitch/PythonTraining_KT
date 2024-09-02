e57c6be4356e9f6b7a60f595a7004e5f66d2b344
WFHStitchWitch <170467935+WFHStitchWitch@users.noreply.github.com>
2024-09-02 13:07:11 -0500

    Renamed dummy module for playing with random number generation. Updated homework problem 3 answers.

--- problem_3.py
+++ problem_3.py
@@ -1,46 +1,66 @@
-'''import random #import Python's Random module from the built-in standard library, because I can't be bothered to make up my own sequences of numbers for this function.'''
+import random
+def generate_string_1(count1, start, end):
+    random.seed(42) #set a fixed seed so the randomizer won't run again every time the code runs. 42 is arbitrary, giving it a seed value.
+    string_1 = []
+    for _ in range(count1):
+        string_1.append(random.randint(start,end))
+    return string_1
+def generate_string_2(count2, start, end):
+    random.seed(13) #set a fixed seed so the randomizer won't run again every time the code runs. 13 is arbitrary, giving it a seed value.
+    string_2 = []
+    for _ in range(count2):
+        string_2.append(random.randint(start,end))
+    return string_2
+count1 = 20
+count2 = 15
+start = -1000
+end = 1001
+string_1 = generate_string_1(count1, start, end)
+string_2 = generate_string_2(count2, start, end)
+print(string_1)
+print(string_2)
+#Works! Have randomized strings of integers, of different lengths.
 
-list(string_1) [1, 2, 20, 22, 44, 99]
-list(string_2) [3, 5, 22, 100, 44, 2] 
-#establish some variables named string_1 and string_2, which are lists, with items in the lists taken out of the example homework problem details.
+#x in s operation on a string to return true/false if 2 is in sequence for each item in the sequence?
+#trying to use enumerate to loop through string_1 and yield elements that equal 2?
+'''def twos_seeker_v1():
+    string_1_collectthetwos = []
+    string_2_collectthetwos = []
+    for index, char in enumerate(string_1):
+        if '2' in char:
+            string_1_collectthetwos.append()
+    for index, char in enumerate(string_2):
+        if '2' in char:
+            string_2_collectthetwos.append()
+            
+            This one did not work
 
-'''random.randint(-1000, 1001) #want to run a random number generator a bunch of times and then add the generated integers into string_1 and also string_2.
+string_1_collectthetwos = twos_seeker
+string_2_collectthetwos = twos_seeker
 
-#s.append(x) operation
+print(string_1_collectthetwos)
+print(string_2_collectthetwos)
 '''
-#need the operations to repeatedly add generated integers to the list variables so they are filled with a different count of figures to play with during the processing of the find_twos function.
 
-#Caution: randint function generates floating point numbers; need to convert them to integers before adding to the lists. 
+#Had the enumerate function and filter function up in the documentation and wasn't sure which one to use. Try with filter function next.
+def twos_seeker_v2():
+    string_1_filtered = []
+    for index, item in enumerate(string_1):
+        if '2' in str(item):
+            string_1_filtered.append(item)
+    return string_1_filtered
+    
+string_1_filtered = twos_seeker_v2()
+print(string_1_filtered)
 
-#x in s operation on a string to return true/false if 2 is in sequence for each item in the sequence? 
+def twos_seeker_v2b():
+    string_2_filtered = []
+    for index, item in enumerate(string_2):
+        if '2' in str(item):
+            string_2_filtered.append(item)
+    return string_2_filtered
+    
+string_2_filtered = twos_seeker_v2b()
+print(string_2_filtered)
+#Not very concise to create two functions that do the same thing, but operate on different strings. However, this works in filtering string 1 into its items that contain the character 2 and the same for string 2.
 
-def find_twos(string_1, string_2):
-    '''
-        This function takes in two strings that only contains integers, commas and whitespace and
-        returns a list of integers, where each integer,
-
-           1. Appears in both strings
-           2. Contains a 2 as a digit in the number.
-
-        Inputs:
-            str1, str2 (string): strings that contains integers, commas, and whitespace. You can assume each integer is separated by a single comma followed by zero or more whitespaces.
-
-        Output:
-            A list of integers, where the list contents is described by above. The returned list must not contain duplicates.
-        '''
-	pass    
----------------------------------------------------------------------
-enumerate(string_1, start=0)
-		two_a = 2
-		for elem in string_1:
-			yield two_a, elem 
-
-'''trying to use enumerate to loop through string_1 and yield elements that equal 2?'''
-
-enumerate(string_2, start=0)
-	two_b=2
-	for elem in string_2:
-		yield two_b, elem
-
-for 2 in string_1 and string_2:
-	return
